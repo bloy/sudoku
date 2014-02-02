9a3af9687b43d4dc5a5da806789c2580e6da03c4
Mike Bloy <mike@bloy.org>
2014-02-02 15:09:21 -0600

    page init

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,3 @@
-//= require_tree .
+// require vendor/jquery-2.1.0.min
+// require vendor/backbone-min
+// require grid
---
+++ source/javascripts/grid.js
@@ -0,0 +1,65 @@
+var grid = grid || {};
+
+grid.Cell = Backbone.Model.extend({
+  defaults: function() {
+    return {
+      value: '',
+      row: '',
+      column: '',
+      group: '',
+      possible: [1,2,3,4,5,6,7,8,9],
+    }
+  },
+});
+
+
+grid.CellCollection = Backbone.Collection.extend({
+  model: grid.Cell
+});
+
+
+grid.CellView = Backbone.View.extend({
+  tagName: 'td',
+  template: _.template($('#cell-template').html()),
+  events: {
+    'click ': 'toggleSelected',
+  },
+
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+    this.input = this.$('.edit');
+    return this;
+  },
+
+  toggleSelected: function() {
+  },
+});
+
+
+grid.AppView = Backbone.View.extend({
+  el: '#gridapp',
+
+
+  initialize: function() {
+    var rowTemplate = _.template('<tr id="grid_row_<%= row %>"></tr>');
+    this.cells = new grid.CellCollection();
+    var cells = this.cells;
+    _.times(9, function(row) {
+      $('#grid').append(rowTemplate({row: row}));
+      _.times(9, function(column) {
+        var cell = new grid.Cell({
+          row: row,
+          column: column,
+          group: (Math.floor(column / 3)) + (Math.floor(row / 3) * 3),
+        });
+        cells.add(cell);
+        var view = new grid.CellView({model: cell});
+        $('#grid_row_' + row).append(view.render().el);
+      });
+    });
+  },
+});
+
+$(function() {
+  App = new grid.AppView();
+});
